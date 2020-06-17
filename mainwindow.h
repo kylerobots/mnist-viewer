bd2097881c275751b3dd2bbc41b03de0fc32a844
Kyle Hart <kyle.m.hart@gmail.com>
2020-06-17 18:08:33 -0400

    Create mock example featuring a number on the screen.

--- mainwindow.h
+++ mainwindow.h
@@ -1,21 +1,25 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include "torch/torch.h"
+
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui {
+	class MainWindow;
+}
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
-    
+class MainWindow : public QMainWindow {
+	Q_OBJECT
+
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
-    
+	MainWindow(QWidget * parent = nullptr);
+	~MainWindow();
+
 private:
-    Ui::MainWindow *ui;
+	Ui::MainWindow * ui;
+	uchar * raw_pixels;
 };
 #endif // MAINWINDOW_H
