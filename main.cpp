8856fc7c3d180667ce7d01ab6b82b4f694ddc097
Kyle Hart <23261132+kylerobots@users.noreply.github.com>
2020-06-21 08:05:04 -0400

    Connect both elements of the gui.

--- main.cpp
+++ main.cpp
@@ -7,6 +7,11 @@ int main(int argc, char * argv[]) {
 	QApplication a(argc, argv);
 	MainWindow w;
 	Network network;
+	// Connect the front end to the back end.
+	a.connect(&w, &MainWindow::iterateImage, &network, &Network::changeImage);
+	a.connect(&network, &Network::example, &w, &MainWindow::displayExample);
+	// The image is only sent when a change is triggered, so manually trigger the first one.
+	network.changeImage(0);
 	w.show();
 	return a.exec();
 }
