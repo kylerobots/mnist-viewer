533a2c685061ef7928d895e61905901a39125cbd
Kyle Hart <23261132+kylerobots@users.noreply.github.com>
2020-06-21 07:53:11 -0400

    Add code to change image index in network.cpp.

--- network.cpp
+++ network.cpp
@@ -1,5 +1,7 @@
 #include "network.h"
 
+#include <iostream>
+
 Network::Network(QObject * parent) :
 		QObject(parent) {
 	data.reset(new torch::data::datasets::MNIST(DATA_PATH.toStdString(), torch::data::datasets::MNIST::Mode::kTest));
@@ -13,8 +15,22 @@ Network::~Network() {
 }
 
 void Network::changeImage(int increment) {
-	// Update the index counter, accounting for int rollover.
-
+	// To avoid overflow, this uses a series of while loops to bring the
+	// requested increment into the range of data values. The assumption
+	// is that the data size won't approach the max value of int.
+	// There are probably ways to do this better, but I was having
+	// difficulty with handling negative increments.
+	size_t data_size = data->size().value();
+	while (increment < 0) {
+		increment += data_size;
+	}
+	while (increment >= data_size) {
+		increment -= data_size;
+	}
+	current_index += increment;
+	while (current_index > data_size) {
+		current_index -= data_size;
+	}
 	sendImage();
 }
 
